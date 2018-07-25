cfc504a100a124a8884fc10b035cc28bd99fa03c
Tee787 <41653444+Tee787@users.noreply.github.com>
2018-07-26 04:46:07 +1200

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // When you start your first line of code, don't forget to add this space between the curly brackets
+            // Just put your cursor infront of the first curly bracket, then press "Enter or Return"
+            // You'll then notice it will automatically indent itself in the new line as it has done
+            // Basic start to coding is to write something
+            // To start we type in a string as follows:
+            Console.WriteLine("Hello World");
+            // In the brackets, between the speech quotes is where you write what you the console to display
+            // You will have noticed "intellisense" pops up to make it easier to code
+            // It will take awhile to get use to it, and then you'll use it quite often
+            // To read our code on the console, we must add this string
+            Console.ReadLine();
+            // At the end of your string, you must remeber the semi-colon
+            // If you forget, it will return an error
+            // Click "Start" or Crtl + F5 to start the console to view what we have written
+            // To close the console, just click the cross (close) tab, top right corner of the console window
+        }
+    }
+}
